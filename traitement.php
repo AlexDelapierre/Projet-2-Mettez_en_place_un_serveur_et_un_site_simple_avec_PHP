69deb83ea5470efc0f50d48db12611c40ff403ff
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-01-16 19:17:11 +0100

    feat: implement artwork insertion into database

---
+++ traitement.php
@@ -0,0 +1,22 @@
+<?php
+
+  require 'bdd.php';
+
+if(empty($_POST['titre'])
+    || empty($_POST['artiste'])
+    || empty($_POST['description'])
+    || empty($_POST['image'])
+    || strlen($_POST['description']) < 3
+    || !filter_var($_POST['image'], FILTER_VALIDATE_URL)) {
+    header('Location: ajouter.php?erreur=true');
+} else {
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+
+    $req = $db->prepare('INSERT INTO oeuvres (titre, description, artiste, image) VALUES (?, ?, ?, ?)');
+    $req->execute([$titre, $description, $artiste, $image]);
+
+    header('Location: oeuvre.php?id=' . $db->lastInsertId());
+}
